07df3dedfd61e0bab8b89b743937ecb9edf4987b
LustySwimmer <419292672@qq.com>
2018-03-06 12:10:33 +0800

    Update LYViewController.m

--- Example/LYFMDBManager/LYViewController.m
+++ Example/LYFMDBManager/LYViewController.m
@@ -2,8 +2,8 @@
 //  LYViewController.m
 //  LYFMDBManager
 //
-//  Created by lvshi on 03/01/2018.
-//  Copyright (c) 2018 lvshi. All rights reserved.
+//  Created by LustySwimmer on 03/01/2018.
+//  Copyright (c) 2018 LustySwimmer. All rights reserved.
 //
 
 #import "LYViewController.h"
