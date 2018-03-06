a0ae40c95b1627207c54aff0796b5e5af6960e51
LustySwimmer <419292672@qq.com>
2018-03-06 12:10:49 +0800

    Update main.m

--- Example/LYFMDBManager/main.m
+++ Example/LYFMDBManager/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  LYFMDBManager
 //
-//  Created by lvshi on 03/01/2018.
-//  Copyright (c) 2018 lvshi. All rights reserved.
+//  Created by LustySwimmer on 03/01/2018.
+//  Copyright (c) 2018 LustySwimmer. All rights reserved.
 //
 
 @import UIKit;
